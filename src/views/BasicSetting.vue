9a4b1b354fe79ff8bd1925eb83ca2105687cc964
ycx <369943318@qq.com>
2020-05-28 14:28:41 +0800

    fix cropper

--- src/views/BasicSetting.vue
+++ src/views/BasicSetting.vue
@@ -74,11 +74,11 @@ export default {
   methods: {
     // 点击裁剪，这一步是可以拿到处理后的地址
     finish () {
-      this.$refs.cropper.getCropBlob(() => {
-        const fd = new FormData();
-        fd.append("avatar", this.fileinfo.raw);
+      this.$refs.cropper.getCropBlob((data) => {
+          const fd = new FormData();
+        fd.append("avatar", data, 'avatar.jpg');
         this.loading = true
-        //上传阿里云服务器
+        //上传服务器
         api.putImg(fd).then(result => {
           this.dialogVisible = false
           this.picsList.push(result.url)
@@ -95,7 +95,6 @@ export default {
         this.$message.error('上传文件大小不能超过 5MB!')
         return false
       }
-      this.fileinfo = file
       // 上传成功后将图片地址赋值给裁剪框显示图片
       this.$nextTick(() => {
         this.option.img = URL.createObjectURL(file.raw)  // 获取URL
@@ -132,15 +131,13 @@ export default {
         outputType: 'jpeg', // 裁剪生成图片的格式
         canScale: false, // 图片是否允许滚轮缩放
         autoCrop: true, // 是否默认生成截图框
-        // autoCropWidth: 300, // 默认生成截图框宽度
-        // autoCropHeight: 200, // 默认生成截图框高度
-        fixedBox: true, // 固定截图框大小 不允许改变
+        fixedBox: false, // 固定截图框大小 不允许改变
         fixed: true, // 是否开启截图框宽高固定比例
-        fixedNumber: [7, 5], // 截图框的宽高比例
-        full: true, // 是否输出原图比例的截图
-        canMoveBox: false, // 截图框能否拖动
+        fixedNumber: [5, 5], // 截图框的宽高比例
+        full: false, // 是否输出原图比例的截图
+        canMoveBox: true, // 截图框能否拖动
         original: false, // 上传图片按照原始比例渲染
-        centerBox: false, // 截图框是否被限制在图片里面
+        centerBox: true, // 截图框是否被限制在图片里面
         infoTrue: true // true 为展示真实输出图片宽高 false 展示看到的截图框宽高
       },
       picsList: [],  //页面显示的数组
